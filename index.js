42dd6d086404b8eb5de937275f8ce5ecb770a796
mbukhman <mbukhman@gmail.com>
2022-10-10 22:02:17 +0400

    modified:   index.html 	modified:   index.js 	new file:   package.json 	new file:   style.css

--- index.js
+++ index.js
@@ -1,50 +1,65 @@
 "use strict";
 
-
 const elements = {
-  'rock': 1,
-  'paper': 2,
-  'scissors': 3
+  rock: 1,
+  paper: 2,
+  scissors: 3,
 };
 
-
 function getRandomElement() {
-  return Math.floor(Math.random() * elements.length)
+  return Math.floor(Math.random() * elements.length);
 }
-
 
 function getUserSelection() {
   let userInput = window.prompt();
 
-  if (!(typeof userInput === "string" && userInput !== '' && userInput !== null)) {
-    return 'error 1';
+  if (
+    !(typeof userInput === "string" && userInput !== "" && userInput !== null)
+  ) {
+    return "error 1";
   }
 
   userInput = userInput.toLowerCase();
 
   if (!elements.hasOwnProperty(userInput)) {
-    return 'error 2'
+    return "error 2";
   }
 
   userElement = elements[userInput];
   return userElement;
 }
 
-
 function getComputerChoice() {
   return getRandomElement();
 }
 
-
-function playRound(playerSelection, computerSelection) {
-  // your code here!
+function getWinner(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "ties";
+  }
+  let countElements = elements.length;
+  let choiceDifference = playerSelection - computerSelection;
+  // Positive Remainder:
+  let choiceDifferenceModulo =
+    choiceDifference % elements.length < 0
+      ? choiceDifference
+      : choiceDifference * -1;
+  if (choiceDifferenceModulo % countElements === 0) {
+  } else {
+  }
 }
 
+function playRound(playerSelection, computerSelection) {
+  let winner = getWinner(playerSelection, computerSelection);
+  return winner;
+}
 
-(function game() {
+function game() {
   for (let i = 0; i < 5; i++) {
-    const playerSelection = getUserSelection();
+    // const playerSelection = getUserSelection();
     const computerSelection = getComputerChoice();
     console.log(playRound(playerSelection, computerSelection));
   }
-})();
+}
+
+game();
